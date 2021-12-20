b4dc6966165f23def424dc1de7251045d1526c6d
FabioHenrique023 <fabiohomeoffice904@gmail.com>
2021-12-20 20:10:39 -0300

    adicionando api

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
-void main() {
+import 'package:http/http.dart' as http;
+Future main() async {
   runApp(const MyApp());
+  final json = await fetch();
+  print(json['results']['name']);
+}
+Future<Map> fetch() async{
+  var url = 'https://randomuser.me/api/';
+  var pessoa = await http.get(Uri.parse(url));
+  var json = jsonDecode(pessoa.body) as Map<String, dynamic>;
+  return json;
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'API',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
